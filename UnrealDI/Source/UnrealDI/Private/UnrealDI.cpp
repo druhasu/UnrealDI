4d2e0dc49b04fabf313e5cdedddc32d033aa4a69
Andrei Sudarikov <druhasu@users.noreply.github.com>
2021-05-13 01:53:23 +0300

    Simplified module definition

--- UnrealDI/Source/UnrealDI/Private/UnrealDI.cpp
+++ UnrealDI/Source/UnrealDI/Private/UnrealDI.cpp
@@ -1,20 +1,5 @@
 // Copyright Andrei Sudarikov. All Rights Reserved.
 
-#include "UnrealDI.h"
+#include "Modules/ModuleManager.h"
 
-#define LOCTEXT_NAMESPACE "FUnrealDIModule"
-
-void FUnrealDIModule::StartupModule()
-{
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-}
-
-void FUnrealDIModule::ShutdownModule()
-{
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
-}
-
-#undef LOCTEXT_NAMESPACE
-	
-IMPLEMENT_MODULE(FUnrealDIModule, UnrealDI)
+IMPLEMENT_MODULE(FDefaultModuleImpl, UnrealDI)
